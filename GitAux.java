2efcf97ce8bcb6b24fe431477fbe5585862e174b
SergioTB <sertineitor@gmail.com>
2015-03-08 18:39:49 +0100

    GitAux.java

--- GitAux.java
+++ GitAux.java
@@ -1,12 +1,7 @@
 package pkgGitHub;
 
 /**
- *
-<<<<<<< HEAD
- * @author alumno: <escribe tu nombre aqui>
-=======
  * @author alumno1: Esther Fernandez Martin
->>>>>>> 9cbc2b759904896479272f33e833a1bcc7ea7d8d
  * @author alumno2: Sergio Tineo Bravo
  * @author alumno3: Luis Fraile Pastrana
  */
@@ -14,45 +9,43 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 System.out.println("modificado por alumno 3"); //alumno 3
-=======
->>>>>>> 404c34d74bd7713fb5fc9ff9b911be0a26e9246c
-=======
+
   	System.out.println("modificado por alumno 1"); // alumno 1
->>>>>>> 9cbc2b759904896479272f33e833a1bcc7ea7d8d
+
     }
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
-<<<<<<< HEAD
+
 	System.out.println("modificado por alumno 2"); // alumno 2
-=======
->>>>>>> 9cbc2b759904896479272f33e833a1bcc7ea7d8d
+
     }
     
     public void metodoAlumno3(){
         System.out.println("metodo 3");
-<<<<<<< HEAD
+
 	// modificaciones alumno 1
 	System.out.println("Alumno 1 inserta esta sentencia");
     }
     
 	
-=======
-    }
+ 
     
     public void metodoComunitario(){
 	//Comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
-<<<<<<< HEAD
-//modificaciones alumno 3
-System.out.println("Alumno 3 inserta esta sentencia");
-=======
+
+	//modificaciones alumno2
+	System.out.println("Alumno 2 inserta esta sentencia");
+
+	//modificaciones alumno 3
+	System.out.println("Alumno 3 inserta esta sentencia");
+
 	// modificaciones alumno 1
 	System.out.println("Alumno 1 inserta esta sentencia");
->>>>>>> 9cbc2b759904896479272f33e833a1bcc7ea7d8d
+
     }
->>>>>>> 9cbc2b759904896479272f33e833a1bcc7ea7d8d
+
 }
