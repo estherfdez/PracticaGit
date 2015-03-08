f8c83b4885a3ed02ac4f98c00c5749dd1a72379f
luisfrap <luisfraipas@gmail.com>
2015-03-08 17:20:26 +0100

    modificaciones alumno 3

--- GitAux.java
+++ GitAux.java
@@ -11,6 +11,7 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+System.out.println("modificado por alumno 3"); //alumno 3
     }
     
     public void metodoAlumno2(){
@@ -23,5 +24,7 @@ public class GitAux {
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+//modificaciones alumno 3
+System.out.println("Alumno 3 inserta esta sentencia");
     }
 }
